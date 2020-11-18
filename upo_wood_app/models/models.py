7f3301fb26d07f99add394799144d031e2157579
Federico <fedevale46@gmail.com>
2020-11-18 12:13:26 +0100

    Relaciones entre clases añadidas

--- upo_wood_app/models/models.py
+++ upo_wood_app/models/models.py
@@ -21,42 +21,54 @@ class Venta(models.Model):
     _name = 'upo_wood_app.venta'
     _description = 'Clase venta para UPOWOOD'
 
-    idVenta = fields.Integer("Numero de venta", required=True)
+    id_venta = fields.Integer("Numero de venta", required=True)
     IVA = fields.Integer("IVA asociado a la venta")
     fechaVenta = fields.Date('Fecha de la venta',required=True, autodate = True)
     total = fields.Integer('Cantidad total de la venta',required=True)
     #Añadir las relaciones entre clases
+    persona_id = fields.Many2many("upo_wood_app.persona")
+    #Relaciones one to one
+    devolucion_ids = fields.Many2one('upo_wood_app.devolucion')
+    envio_ids = fields.Many2one('upo_wood_app.envio')
+    factura_id = fields.Many2one("upo_wood_app.factura")
+    producto_id = fields.One2many('upo_wood_app.producto')
+    
 
 
 class Devolucion(models.Model):
     _name = 'upo_wood_app.devolucion'
     _description = 'Clase devolucion para UPOWOOD'
 
+    id_devolucion = fields.Integer('Numero de devolucion', required=True)
     motivo = fields.Text('Descripcion del motivo de la devolucion',required=True)
     isAprobado = fields.Boolean("Devolucion aprobada o denegada")
     fechaDevolucion = fields.Date('Fecha de la devolucion',required=True, autodate = True)
     #Añadir las relaciones entre clases
-
+    venta_id = fields.Many2one(string='', comodel_name='', ondelete='restrict')
 
 class Envio(models.Model):
     _name = 'upo_wood_app.envio'
     _description = 'Clase envio para UPOWOOD'
 
-    idEnvio = fields.Integer("Numero de envio", required=True)
+    id_envio = fields.Integer("Numero de envio", required=True)
     direccion = fields.Text("Direccion del envio",required=True)
     cp = fields.Integer('Codigo postal',required=True)
     fechaEnvio = fields.Date('Fecha de envio',required=True, autodate = True)
     fechaLlegada = fields.Date('Fecha de llegada prevista',required=True)
     #Añadir las relaciones entre clases
+    venta_id = fields.Many2one('upo_wood_app.venta')
+    albaran_id = fields.Many2one('upo_wood_app.albaran')
 
 class Albaran(models.Model):
     _name = 'upo_wood_app.albaran'
     _description = 'Clase albaran para UPOWOOD'
 
+    id_albaran = fields.Integer("Numero de albaras", required=True)
     isEntregado = fields.Boolean("Se ha realizado el envio correctamente?",required=True)
     fechaLlegada = fields.Date('Fecha de llegada prevista',required=True)
     descripcion = fields.Text("Descripcion de la llegada y, en caso de problemas, motivo de no haber realizado la entrega")
     #Añadir las relaciones entre clases
+    envio_id = fields.Many2one('upo_wood_app.venta')
 
 class Persona(models.Model):
     _name = 'upo_wood_app.persona'
@@ -73,44 +85,53 @@ class Persona(models.Model):
     foto = fields.Binary("Foto usuario")
     isEmpleado = fields.Boolean("El usuario es empleado o cliente?",required=True)
     #Añadir las relaciones entre clases
+    venta_ids = fields.Many2many('upo_wood_app.venta')
 
 class Factura(models.Model):
     _name = 'upo_wood_app.factura'
     _description = 'Clase factura para UPOWOOD'
 
-    idFactura = fields.Integer("Numero de factura", required=True)
+    id_factura = fields.Integer("Numero de factura", required=True)
     fecha = fields.Date('Fecha de la factura',required=True, autodate = True)
     #Añadir las relaciones entre clases
+    venta_id = fields.Many2one('upo_wood_app.venta')
 
 class Producto(models.Model):
     _name = 'upo_wood_app.producto'
     _description = 'Clase producto para UPOWOOD'
 
-    idProducto = fields.Integer("Numero del producto", required=True)
+    id_producto = fields.Integer("Numero del producto", required=True)
     fechaFabricacion = fields.Date('Fecha de la fabricacion del producto',required=True)
     precio = fields.Integer("Precio del producto",required=True)
+    stock = fields.Integer("Stock del producto",required=True)
+    foto = fields.Binary("FotoProducto")
     #Añadir las relaciones entre clases
+    venta_id = fields.Many2many('upo_wood_app.venta')
+    ##Completar a partir de aqui
+    categoria_id = fields.Many2one('upo_wood_app.categoria')
+    material_id = fields.Many2one('upo_wood_app.material')
 
-class Modelo(models.Model):
+class Categoria(models.Model):
     _name = 'upo_wood_app.modelo'
     _description = 'Clase modelo para UPOWOOD'
 
     nombre = fields.Char(string="Nombre del modelo",required=True)
     descripcion = fields.Text("Descripcion del modelo en cuestion")
-    stock = fields.Integer("Stock del modelo",required=True)
-    foto = fields.Binary("FotoModelo")
     #Añadir las relaciones entre clases
+    producto_ids = fields.Many2many('upo_wood_app.producto')
 
 class Material(models.Model):
     _name = 'upo_wood_app.material'
     _description = 'Clase material para UPOWOOD'
 
-    idMaterial = fields.Integer("Numero del material", required=True)
+    id_material = fields.Integer("Numero del material", required=True)
     nombre = fields.Char(string="Nombre del material",required=True)
     descripcion = fields.Text("DDescripcion del material en cuestion")
     precio = fields.Integer("Precio del material",required=True)
     foto = fields.Binary("FotoMaterial")
     #Añadir las relaciones entre clases
+    producto_ids = fields.Many2many('upo_wood_app.producto')
+    proveedor_ids = fields.Many2many('upo_wood_app.proveedor')
 
  class Proveedor(models.Model):
     _name = 'upo_wood_app.proveedor'
@@ -121,3 +142,4 @@ class Material(models.Model):
     cif =fields.Char(string="CIF del proveedor",required=True)
     telefono = fields.Integer("Numero de telefono")
     #Añadir las relaciones entre clases  
+    material_ids = fields.Many2many('upo_wood_app.material')
