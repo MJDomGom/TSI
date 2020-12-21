d50bc7e88e3845507659e2f4903128c378952ae1
Manuel Jesus <mjdomgom@gmail.com>
2020-12-21 20:45:51 +0100

    Update
    Añadido workflows en envio y devolucion y falta que los reports de producto y factura funcionen

--- upo_wood_app/models/devolucion.py
+++ upo_wood_app/models/devolucion.py
@@ -8,6 +8,14 @@ class Devolucion(models.Model):
     motivo = fields.Text('Descripcion del motivo de la devolucion',required=True)
     isAprobado = fields.Boolean("Devolucion aprobada o denegada")
     fechaDevolucion = fields.Date('Fecha de la devolucion',required=True, autodate = True)
+    state = fields.Selection([('aprobado','Aprobado'),('denegado','Denegado'),('pendiente','Pendiente')],'Estado',default='pendiente')
     #Añadir las relaciones entre clases
     venta_id = fields.Many2one("upo_wood_app.venta",string="Venta relacionada con la devolucion")
     _sql_constraints = [('devolucion_name_unique','UNIQUE (name)','El número de la devolución debe ser único')]
+
+    @api.constrains('isAprobado')
+    def btn_submit_to_isAprobado(self):
+        if self.isAprobado == True:
+            self.write({'state':'aprobado'})
+        else:
+            self.write({'state':'denegado'})
--- upo_wood_app/models/envio.py
+++ upo_wood_app/models/envio.py
@@ -9,7 +9,24 @@ class Envio(models.Model):
     cp = fields.Integer('Codigo postal',required=True)
     fechaEnvio = fields.Date('Fecha de envio',required=True, autodate = True)
     fechaLlegada = fields.Date('Fecha de llegada prevista',required=True)
+    state = fields.Selection([('preparando','Preparando'),('enviado','Enviado'),('reparto','En reparto'),('entregado','Entregado'),('cancelado','Cancelado')],'Estado',default='preparando')
     #Añadir las relaciones entre clases
     venta_id = fields.Many2one("upo_wood_app.venta",string="Venta relacionada con el envio")
     albaran_id = fields.Many2one("upo_wood_app.albaran",string="Albaran relacionado con el envio")
     _sql_constraints = [('envio_name_unique','UNIQUE (name)','El número del envío debe ser único')]
+
+    def btn_submit_to_preparando(self):
+        self.write({'state':'preparando'})
+    
+    def btn_submit_to_enviado(self):
+        self.write({'state':'enviado'})
+    
+    def btn_submit_to_reparto(self):
+        self.write({'state':'reparto'})
+
+    def btn_submit_to_entregado(self):
+        self.write({'state':'entregado'})
+
+    def btn_submit_to_cancelado(self):
+        self.write({'state':'cancelado'}) 
+    
